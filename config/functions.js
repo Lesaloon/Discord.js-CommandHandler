8b5f09777bc828a9c7544cfe8573c79dc443613c
Senchuu <raph.corrotte@gmail.com>
2020-07-27 11:48:57 +0200

    Add colors.js and functions.js

--- config/functions.js
+++ config/functions.js
@@ -5,20 +5,20 @@ const client = require("../main.js")
  * @param {*} permission - Permission required
  * @param {*} message - Permission message
  */
-export function getPermission(user, permission, message) {
+exports.getPermission = function(user, permission, message) {
     return message.member(user).hasPermission(permission) || false;
 };
 /**
  * @function isOwner - Get if user is in owner's list
  * @param {*} id - The id of the targeted user
  */
-export function isOwner(id) {
+exports.isOwner = function(id) {
     return client.OWNERS.includes(id) || false;
 };
 /**
  * @function getCommand - Get a client command
  * @param {*} nameOrAlias - The name or the alias of the targeted command
  */
-export function getCommand(nameOrAlias) {
+exports.getCommand = function(nameOrAlias) {
     return client.commands.find((c) => c.name === nameOrAlias) || client.commands.find((c) => c.aliases !== undefined && c.aliases.includes(name)) || false;
 };
