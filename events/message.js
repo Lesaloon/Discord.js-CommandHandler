c5fdf9ea249aab83b1d305e0552cf62c99bf50b4
Senchuu <raph.corrotte@gmail.com>
2020-07-28 20:52:12 +0200

    Update message.js with error function and ready.js with moment

--- events/message.js
+++ events/message.js
@@ -1,3 +1,31 @@
+const {getCommand, getPermission, discordEmbed} = require("../config/functions.js");
 module.exports = async(client, message) => {
-    
-}
+    let commandPrefix;
+    for(const prefix of client.prefixes) {
+        if(message.content.startsWith(prefix)) {
+            commandPrefix = prefix;
+            client.prefix = commandPrefix;
+        };
+    };
+    if(!commandPrefix) return;
+    let args = message.content.slice(commandPrefix.length).split(/ +/g);
+    let command = getCommand(args[0]);
+    if(command) {
+        command.run(client, message, args)
+            .catch((err) => message.channel.send(errorFunction(err)));
+    };
+};
+
+function errorFunction(err) {
+    return discordEmbed({
+        description: `
+        **Error in command** : 
+        \`\`\`js
+        ${err}
+        \`\`\`  
+        **Node Version**: \`${process.version}\`
+        **Librairy version**: \`${require("../package.json").dependencies["discord.js"]}\`
+        `
+    })
+    .setColor(client.hexColors.red);
+};
--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const {redBright} = require("chalk")
 module.exports = async(client) => {
     console.log(redBright(`Ready in ${client.users.cache.size} users & ${client.guilds.cache.size} guilds`));
+    client.prefixes.push(`<@${client.user.id}>`)
 };
